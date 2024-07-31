19475453fe6596683ce0c4ce5a68002bc7376b40
Paul <157764547+Safaero@users.noreply.github.com>
2024-07-31 15:16:12 -0600

    test login added
    (paul)

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    SQL_SERVER = 'JOSEDELACRUZ25'
+    SQL_SERVER = 'DESKTOP-U16RP4E\\SQLEXPRESS'
     DATABASE = 'Guiatu1'
-    DRIVER = 'SQL Server'
+    DRIVER = 'ODBC Driver 17 for SQL Server'
     CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};DATABASE={DATABASE};Trusted_Connection=yes;'
     SECRET_KEY = 'your_secret_key'
