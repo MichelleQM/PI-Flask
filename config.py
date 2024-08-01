674c7ad8bc5da703e714965fbf8ab44bd88da758
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2024-07-31 19:31:26 -0600

    1

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 class Config:
+<<<<<<< Updated upstream
     SQL_SERVER = 'DESKTOP-U16RP4E\\SQLEXPRESS'
+=======
+    SQL_SERVER = 'DESKTOP-CQ8T8UT'
+>>>>>>> Stashed changes
     DATABASE = 'Guiatu1'
     DRIVER = 'ODBC Driver 17 for SQL Server'
     CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};DATABASE={DATABASE};Trusted_Connection=yes;'
