8edb7bcec86ba9c386a688b603c47fbf6f779806
Lot Mondragon <139500449+122043254@users.noreply.github.com>
2024-08-05 10:45:20 -0600

    falto este

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    SQL_SERVER = 'DESKTOP-U16RP4E\\SQLEXPRESS'
-    DATABASE = 'Guiatu1'
-    DRIVER = 'ODBC Driver 17 for SQL Server'
+    SQL_SERVER = 'PC-LOT'
+    DATABASE = 'Guiatu2'
+    DRIVER = 'SQL Server'
     CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};DATABASE={DATABASE};Trusted_Connection=yes;'
     SECRET_KEY = 'your_secret_key'
